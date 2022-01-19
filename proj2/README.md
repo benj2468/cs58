fa8c7650ebcd13fdbd4a00cf51877be16289dd76
Benjamin Cape <benjcape@gmail.com>
2022-01-18 23:24:51 -0500

    using waiting queue

---
+++ proj2/README.md
@@ -0,0 +1,9 @@
+# Programming Project 2: Ledyard Bridge
+
+## Running the Program
+
+```
+make ledyard && ./ledyard
+```
+
+You should pipe in a string of 1s and 0s that indicate the destination of the cars as they arrive at the bridge. I.E. 0101 indicates that a car destined for Hanover arrives, then a car for Norwich, then Hanover, then Norwich.
--- proj2/main.c
+++ proj2/main.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -21,11 +22,23 @@ struct Car
     enum Direction direction;
 };
 
+struct ListItem
+{
+    int id;
+    struct ListItem *next;
+};
+
+struct List
+{
+    struct ListItem *head;
+    struct ListItem *tail;
+};
+
 struct Bridge
 {
     pthread_mutex_t lock;
     int cars;
-    int waiting[2];
+    struct List *waiting[2];
     enum Direction direction;
 };
 
@@ -36,24 +49,71 @@ void ArriveBridge(struct Car *car)
 {
     int id = car->id;
     enum Direction dir = car->direction;
+    int am_waiting = 0;
+
     while (1)
     {
         pthread_mutex_lock(&ledyard->lock);
-        if (ledyard->cars < MAX_CARS && ledyard->direction == dir)
+        /*
+        Reqs:
+        - cars going opposite directions crash on the bridge -- ensured by checking direction here, and switching direction below
+        - the bridge collapses, because too many cars were on it -- ensured by making sure less than MAX_CARS are on bridge
+        */
+
+        if ((am_waiting && ledyard->waiting[dir]->head->id == id) || !am_waiting)
         {
-            printf("[%d] Car Entering \n", id);
-            ledyard->cars++;
-            pthread_mutex_unlock(&ledyard->lock);
-            break;
+            if (ledyard->cars < MAX_CARS && ledyard->direction == dir)
+            {
+                if (am_waiting)
+                {
+                    ledyard->waiting[dir]->head = ledyard->waiting[dir]->head->next;
+                    if (ledyard->waiting[dir]->head == NULL)
+                    {
+                        ledyard->waiting[dir]->tail = NULL;
+                    }
+                }
+                // printf("[%d] Car Entering \n", id);
+                ledyard->cars++;
+                pthread_mutex_unlock(&ledyard->lock);
+                break;
+            }
+            else if (ledyard->cars == 0 && ledyard->direction != dir)
+            // Cars don't crash here because we have 0 cars going the other direction, so switching direction is OK.
+            {
+                if (am_waiting)
+                {
+                    ledyard->waiting[dir]->head = ledyard->waiting[dir]->head->next;
+                    if (ledyard->waiting[dir]->head == NULL)
+                    {
+                        ledyard->waiting[dir]->tail = NULL;
+                    }
+                }
+                // printf("[%d] Car Entering, switching dir \n", id);
+                ledyard->direction = dir;
+                ledyard->cars++;
+                pthread_mutex_unlock(&ledyard->lock);
+                break;
+            }
         }
-        else if (ledyard->cars == 0 && ledyard->direction != dir)
+
+        // Car cannot get on now... so we have to add it to the queue.
+        // if (!am_waiting)
+        //     printf("[%d] Arrived and is waiting\n", id);
+        am_waiting = 1;
+        struct ListItem *car_item = malloc(sizeof(struct ListItem));
+        car_item->id = id;
+        if (ledyard->waiting[dir]->tail == NULL)
         {
-            printf("[%d] Car Entering, switching dir \n", id);
-            ledyard->direction = dir;
-            ledyard->cars++;
-            pthread_mutex_unlock(&ledyard->lock);
-            break;
+            ledyard->waiting[dir]->tail = car_item;
+            ledyard->waiting[dir]->head = car_item;
         }
+        else
+        {
+
+            ledyard->waiting[dir]->tail->next = car_item;
+            ledyard->waiting[dir]->tail = car_item;
+        }
+
         pthread_mutex_unlock(&ledyard->lock);
         sleep(1);
     }
@@ -80,6 +140,19 @@ void OnBridge(struct Car *car)
         direction = "To Hanover";
     }
 
+    int waits[2];
+    for (int i = 0; i < 2; i++)
+    {
+        if (ledyard->waiting[i]->head != NULL)
+        {
+            waits[i] = ledyard->waiting[i]->head->id;
+        }
+        else
+        {
+            waits[i] = NULL;
+        }
+    }
+
     printf("\n\
     |------- [%d] Ledyard Bridge -------|\n\
     |------------------------------|\n\
@@ -88,7 +161,7 @@ void OnBridge(struct Car *car)
     | Waiting (Hanover): %d         |\n\
     | Waiting (Norwich): %d         |\n\
     |------------------------------|\n",
-           id, direction, ledyard->cars, ledyard->waiting[0], ledyard->waiting[1]);
+           id, direction, ledyard->cars, waits[0], waits[1]);
 
     pthread_mutex_unlock(&ledyard->lock);
 
@@ -101,7 +174,7 @@ void ExitBridge(struct Car *car)
     enum Direction dir = car->direction;
     pthread_mutex_lock(&ledyard->lock);
 
-    printf("[%d] Car Leaving\n", id);
+    // printf("[%d] Car Leaving\n", id);
 
     ledyard->cars--;
 
@@ -119,22 +192,15 @@ void *OneVehicle(void *arg)
 
     ExitBridge(car);
     // now the car is off
+
+    return NULL;
 }
 
 int main(int argc, char **argv)
 {
 
-    if (argc < 3)
-    {
-        perror("Not enough arguments");
-        exit(101);
-    }
-
-    int waiting[2] = {atoi(argv[1]), atoi(argv[2])};
-
     ledyard = malloc(sizeof(struct Bridge));
-
-    int total_cars = waiting[0] + waiting[1];
+    int total_cars = 1;
     pthread_t *all_threads = malloc(total_cars * sizeof(pthread_t));
 
     // Ledyard Initialization
@@ -144,24 +210,43 @@ int main(int argc, char **argv)
         return 1;
     }
     ledyard->cars = 0;
+    ledyard->waiting[0] = malloc(sizeof(struct List));
+    ledyard->waiting[1] = malloc(sizeof(struct List));
     ledyard->direction = TO_HANOVER;
-
     // Initialization Complete
 
-    int i;
-    for (i = 0; i < waiting[0]; i++)
+    int c;
+    int i = 0;
+    char ch;
+
+    while (1)
     {
+        c = getchar();
+        ch = (char)c;
+        if (ch == '\n')
+            break;
+
+        if (i == total_cars)
+        {
+
+            total_cars = total_cars * 2;
+            pthread_t *new = malloc(total_cars * sizeof(pthread_t));
+            memcpy(new, all_threads, total_cars / 2);
+            all_threads = new;
+        }
+
+        if (ch != '0' && ch != '1') // To Hanover
+        {
+            printf("%c\n", ch);
+            perror("Invalid Character");
+            exit(102);
+        }
+
         struct Car *car = malloc(sizeof(struct Car));
         car->id = i;
-        car->direction = TO_HANOVER;
+        car->direction = (int)atoi(&ch);
         pthread_create(&all_threads[i], NULL, OneVehicle, (void *)car);
-    }
-    for (i = i; i < total_cars; i++)
-    {
-        struct Car *car = malloc(sizeof(struct Car));
-        car->id = i;
-        car->direction = TO_NORWICH;
-        pthread_create(&all_threads[i], NULL, OneVehicle, (void *)car);
+        i++;
     }
 
     for (int k = 0; k < total_cars; k++)
