401f3ad25bec0f851f484301e15c547816e65e32
Benjamin Cape <benjcape@gmail.com>
2022-01-20 20:45:19 -0500

    driver for testing

--- proj2/ledyard.c
+++ proj2/ledyard.c
@@ -1,11 +1,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
 
 #define MAX_CARS 3
 #define TIME_TO_CROSS 1
@@ -39,9 +41,9 @@ void ArriveBridge(struct Car *car)
     int id = car->id;
     enum Direction dir = car->direction;
 
+    pthread_mutex_lock(&ledyard->lock);
     while (1)
     {
-        pthread_mutex_lock(&ledyard->lock);
         /*
         Reqs:
         - cars going opposite directions crash on the bridge -- ensured by checking direction here, and switching direction below
@@ -49,26 +51,24 @@ void ArriveBridge(struct Car *car)
         */
         if (ledyard->cars < MAX_CARS && ledyard->direction == dir)
         {
-
-            printf("[%d] Car Entering \n", id);
+            printf("[%d] Car Entering \n\n", id);
             ledyard->cars++;
-            pthread_mutex_unlock(&ledyard->lock);
             break;
         }
         else if (ledyard->cars == 0 && ledyard->direction != dir)
         // Cars don't crash here because we have 0 cars going the other direction, so switching direction is OK.
         {
-            printf("[%d] Car Entering, switching dir \n", id);
+            printf("[%d] Car Entering, switching dir \n\n", id);
             ledyard->direction = dir;
             ledyard->cars++;
-            pthread_mutex_unlock(&ledyard->lock);
             break;
         }
+        printf("[%d] Car Waiting \n\n", id);
         ledyard->waiting[dir]++;
         pthread_cond_wait(&ledyard->cvar, &ledyard->lock);
         ledyard->waiting[dir]--;
-        pthread_mutex_unlock(&ledyard->lock);
     }
+    pthread_mutex_unlock(&ledyard->lock);
 }
 
 // OnBridge should, as a side-effect, print the state of the bridge and waiting cars, in some nice format, to make it easier to monitor the behavior of the system.
@@ -113,15 +113,16 @@ void ExitBridge(struct Car *car)
     enum Direction dir = car->direction;
     pthread_mutex_lock(&ledyard->lock);
 
-    // printf("[%d] Car Leaving\n", id);
+    printf("[%d] Car Leaving\n\n", id);
 
     ledyard->cars--;
 
-    pthread_cond_signal(&ledyard->cvar);
+    pthread_cond_broadcast(&ledyard->cvar);
 
     pthread_mutex_unlock(&ledyard->lock);
 }
 
+// Thread Main
 void *OneVehicle(void *arg)
 {
 
@@ -137,60 +138,60 @@ void *OneVehicle(void *arg)
     return NULL;
 }
 
-int main(int argc, char **argv)
+// Actual Simulation
+int simulate(int schedule[][2], int times)
 {
 
     ledyard = malloc(sizeof(struct Bridge));
-    int total_cars = 1;
-
-    pthread_t **all_threads = (pthread_t **)malloc(total_cars * sizeof(pthread_t *));
 
     // Ledyard Initialization
+    // Initialize lock
     if (pthread_mutex_init(&ledyard->lock, NULL) != 0)
     {
         printf("\n mutex init has failed\n");
         return 1;
     }
+    // Initialize cvar
     if (pthread_cond_init(&ledyard->cvar, NULL) != 0)
     {
         printf("\n cvar init failed\n");
         return 1;
     }
+    // Initialize bridge
     ledyard->cars = 0;
     ledyard->direction = TO_HANOVER;
     // Initialization Complete
 
-    int c;
-    int i = 0;
-    char ch;
+    int total_cars = 0;
 
-    while (1)
+    for (int i = 0; i < times; i++)
     {
-        c = getchar();
-        ch = (char)c;
-        if (ch == '\n')
-            break;
+        total_cars += schedule[i][0] + schedule[i][1];
+    }
 
-        if (i == total_cars)
+    pthread_t all_threads[total_cars];
+
+    for (int i = 0; i < times; i++)
+    {
+
+        for (int j = 0; j < schedule[i][0]; j++)
         {
-            total_cars = total_cars * 2;
-
-            all_threads = (pthread_t **)realloc(all_threads, (total_cars) * sizeof(pthread_t *));
+            int id = i + j;
+            struct Car *car = malloc(sizeof(struct Car));
+            car->id = id;
+            car->direction = 0;
+            pthread_create(&all_threads[id], NULL, OneVehicle, (void *)car);
+        }
+        for (int k = 0; k < schedule[i][1]; k++)
+        {
+            int id = i + schedule[i][0] + k;
+            struct Car *car = malloc(sizeof(struct Car));
+            car->id = id;
+            car->direction = 1;
+            pthread_create(&all_threads[id], NULL, OneVehicle, (void *)car);
         }
 
-        if (ch != '0' && ch != '1') // To Hanover
-        {
-            printf("%c\n", ch);
-            perror("Invalid Character");
-            exit(102);
-        }
-
-        struct Car *car = malloc(sizeof(struct Car));
-        car->id = i;
-        car->direction = (int)atoi(&ch);
-
-        pthread_create(&all_threads[i], NULL, OneVehicle, (void *)car);
-        i++;
+        sleep(1);
     }
 
     int k = 0;
@@ -200,5 +201,85 @@ int main(int argc, char **argv)
         k++;
     }
 
+    pthread_mutex_destroy(&ledyard->lock);
+    pthread_cond_destroy(&ledyard->cvar);
+
     return 0;
+}
+
+// Helper to generate a schedule
+void make_schedule(int times, int schedule[times][2])
+{
+    for (int i = 0; i < times; i++)
+    {
+        int op = rand() % 4;
+        int t[2] = {rand() % 10, rand() % 10};
+
+        if (op == 0)
+        {
+            t[0] = 0;
+        }
+        else if (op == 1)
+        {
+            t[1] = 0;
+        }
+        schedule[i][0] = t[0];
+        schedule[i][1] = t[1];
+    }
+}
+
+// Helper to print the schedule
+void print_schedule(int times, int schedule[times][2])
+{
+    printf("Schedule:\n---------------\n");
+    for (int i = 0; i < times; i++)
+    {
+        printf("---- [t = %d] Norwich: %d; Hanover: %d\n", i, schedule[i][0], schedule[i][1]);
+    }
+    printf("---------------\n");
+}
+
+// Driver for a random test & Simulation
+void driver(char *out_file)
+{
+    int times = rand() % 5;
+    int schedule[times][2];
+    make_schedule(times, schedule);
+
+    freopen(out_file, "w", stdout);
+
+    printf("```\n");
+
+    print_schedule(times, schedule);
+
+    simulate(schedule, times);
+
+    printf("```");
+
+    fclose(stdout);
+}
+
+int main(int argc, char **argv)
+{
+    srand(time(NULL));
+
+    int rc = fork();
+    if (rc == 0)
+    {
+        execlp("mkdir", "mkdir", "-p", "./logs", NULL);
+    }
+
+    int tests = 1;
+    if (argc > 1)
+    {
+        tests = atoi(argv[1]);
+    }
+
+    for (int i = 1; i <= tests; i++)
+    {
+        int digits = (int)ceil(log10(i));
+        char output_file[11 + digits];
+        sprintf(output_file, "./logs/test%d", i);
+        driver(output_file);
+    }
 }
